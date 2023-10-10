253c1c3500d15fcba5ccbe38d942c3cf650dc6cb
Vespertino <Vespertino@C09PC02.informatica.comercio>
2023-10-10 21:45:19 +0200

    First commit

---
+++ Tema 2/Tareas/Tarea.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tarea
+{
+    string descripcion;
+    DateTime fechaVencimiento;
+    bool completada;
+    public Tarea(string descripcion,int año, int mes, int dia)
+    {
+        this.descripcion = descripcion;
+        this.fechaVencimiento = new DateTime(año, mes, dia);
+        completada = false;
+    }
+    public String toString()
+    {
+        return "Descripcion: " + descripcion + " Fecha: " + fechaVencimiento + " Completada: " + completada;
+    }
+    
+    
+    public bool EstaCompletada()
+    {
+        return completada;
+    }
+    public DateTime getFecha()
+    {
+        return fechaVencimiento;
+    }
+    public void MarcarComoCompletada()
+    {
+        completada = true;
+    }
+}
